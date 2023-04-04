df6d67ada5fdac8890882afb61b683e9159cc52b
donghyunHong <tlxmscksals@naver.com>
2023-04-04 12:45:13 +0900

    04.04

---
+++ Day01/Day01_variable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day01_variable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte bdata = byte.MaxValue;
+            Console.WriteLine(bdata);
+
+            bdata = byte.MinValue; 
+            Console.WriteLine(bdata);
+
+            long ldata = long.MaxValue;
+            Console.WriteLine(ldata);
+            ldata = long.MinValue;
+            Console.WriteLine(ldata);
+        }
+    }
+}
